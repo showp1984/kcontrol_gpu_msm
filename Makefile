38c86a35de16cfd75a0b4f93833ea9c2324c41c3
Dennis Rassmann <showp1984@gmail.com>
2013-04-07 06:35:18 +0200

    update to latest version
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
-KERNEL_BUILD := /home/showp1984/WORK/kernel/bricked-mako/
+KERNEL_BUILD := /home/showp1984/WORK/kernel/3.4.0-modbuild-kernel-aosp/
 
 obj-m += kcontrol_gpu_msm.o
 
 all:
 	make -C $(KERNEL_BUILD) M=$(PWD) modules
-	$(KERNEL_CROSS_COMPILE)strip --strip-debug kcontrol_gpu_msm.ko
+	$(CROSS_COMPILE)strip --strip-debug kcontrol_gpu_msm.ko
 
 clean:
 	make -C $(KERNEL_BUILD) M=$(PWD) clean 2> /dev/null
--- kcontrol_gpu_msm.c
+++ kcontrol_gpu_msm.c
@@ -23,6 +23,8 @@
 #include <linux/errno.h>
 #include <linux/kallsyms.h>
 #include <linux/sysfs.h>
+#include <mach/kgsl.h>
+#include <linux/platform_device.h>
 
 #define THIS_EXPERIMENTAL 1
 
@@ -36,32 +38,86 @@ MODULE_DESCRIPTION(DRIVER_DESCRIPTION);
 MODULE_VERSION(DRIVER_VERSION);
 MODULE_LICENSE("GPL");
 
-/* module parameters */ //no need atm
-//static uint enabled = 1;
-//module_param(enabled, uint, 0444);
+struct global_attr {
+	struct attribute attr;
+	ssize_t (*show)(struct kobject *kobj,
+			struct attribute *attr, char *buf);
+	ssize_t (*store)(struct kobject *a, struct attribute *b,
+			 const char *c, size_t count);
+};
+
+#define define_one_global_ro(_name)		\
+static struct global_attr _name =		\
+__ATTR(_name, 0444, show_##_name, NULL)
+
+/* module parameters */
+static uint kgsl_pdata = 0x00000000;
+module_param(kgsl_pdata, uint, 0444);
+
+static uint kgsl_3d_dev = 0x00000000;
+module_param(kgsl_3d_dev, uint, 0444);
+
+struct kgsl_device_platform_data *kpdata;
+struct platform_device *kpdev;
+
+struct kobject *kcontrol_gpu_msm_kobject;
+static ssize_t show_kgsl_pwrlevels(struct kobject *a, struct attribute *b,
+				   char *buf)
+{
+	int i = 0, len = 0;
+	for (i=0; i<ARRAY_SIZE(kpdata->pwrlevel); i++) {
+		len += sprintf(buf + len, "%u ", kpdata->pwrlevel[i].gpu_freq);
+	}
+	return len;
+}
+
+define_one_global_ro(kgsl_pwrlevels);
+
+static struct attribute *kcontrol_gpu_msm_attributes[] = {
+	&kgsl_pwrlevels.attr,
+	NULL
+};
+
+static struct attribute_group kcontrol_gpu_msm_attr_group = {
+	.attrs = kcontrol_gpu_msm_attributes,
+	.name = "gpu_msm",
+};
 
 static int __init kcontrol_gpu_msm_init(void)
 {
-
+	int rc = 0;
+	kpdata = (struct kgsl_device_platform_data *)kgsl_pdata;
+	kpdev = (struct platform_device *)kgsl_3d_dev;
 
 #if THIS_EXPERIMENTAL
-        printk(KERN_WARNING LOGTAG "#######################################");
-        printk(KERN_WARNING LOGTAG "WARNING: THIS MODULE IS EXPERIMENTAL!\n");
-        printk(KERN_WARNING LOGTAG "You have been warned.\n");
+    printk(KERN_WARNING LOGTAG "#######################################");
+    printk(KERN_WARNING LOGTAG "WARNING: THIS MODULE IS EXPERIMENTAL!\n");
+    printk(KERN_WARNING LOGTAG "You have been warned.\n");
 	printk(KERN_INFO LOGTAG "%s, version %s\n", DRIVER_DESCRIPTION,
-		DRIVER_VERSION);
+	DRIVER_VERSION);
 	printk(KERN_INFO LOGTAG "author: %s\n", DRIVER_AUTHOR);
-        printk(KERN_WARNING LOGTAG "#######################################");
+    printk(KERN_WARNING LOGTAG "#######################################");
 #else
-        printk(KERN_INFO LOGTAG "%s, version %s loaded\n", DRIVER_DESCRIPTION,
-               DRIVER_VERSION);
-        printk(KERN_INFO LOGTAG "by: %s\n", DRIVER_AUTHOR);
+    printk(KERN_INFO LOGTAG "%s, version %s loaded\n", DRIVER_DESCRIPTION,
+           DRIVER_VERSION);
+    printk(KERN_INFO LOGTAG "by: %s\n", DRIVER_AUTHOR);
 #endif
-        BUG_ON(old_kobj == NULL);
+
+    WARN_ON(kpdata = NULL);
+    WARN_ON(kpdev = NULL);
+
+	kcontrol_gpu_msm_kobject = kobject_create_and_add("kcontrol", kernel_kobj);
+	if (kcontrol_gpu_msm_kobject) {
+		rc = sysfs_create_group(kcontrol_gpu_msm_kobject,
+							&kcontrol_gpu_msm_attr_group);
+		if (rc) {
+			pr_warn(LOGTAG"sysfs: ERROR, could not create sysfs group");
+		}
+	} else
+		pr_warn(LOGTAG"sysfs: ERROR, could not create sysfs kobj");
 
 
-
-        printk(KERN_INFO LOGTAG "all done, have fun!\n");
+    printk(KERN_INFO LOGTAG "everything done, have fun!\n");
 	return 0;
 }
 
